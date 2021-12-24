bd15764c51021f29504d7f1321d733d7c0bce20c
coderSuresh <sd807141@gmail.com>
2021-12-24 18:51:07 +0545

    added

--- DaysOfWeek.java
+++ DaysOfWeek.java
@@ -13,22 +13,22 @@ public class DaysOfWeek {
                 System.out.println("Sunday");
                 break;
             case 2:
-                System.out.println("Sunday");
+                System.out.println("Monday");
                 break;
             case 3:
-                System.out.println("Sunday");
+                System.out.println("Tuesday");
                 break;
             case 4:
-                System.out.println("Sunday");
+                System.out.println("Wednesday");
                 break;
             case 5:
-                System.out.println("Sunday");
+                System.out.println("Thursday");
                 break;
             case 6:
-                System.out.println("Sunday");
+                System.out.println("Friday");
                 break;
             case 7:
-                System.out.println("Sunday");
+                System.out.println("Saturday");
                 break;
             default:
                 System.out.println("Invalid number");
@@ -37,6 +37,7 @@ public class DaysOfWeek {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
         int x = sc.nextInt();
         DaysOfWeek days = new DaysOfWeek();
         days.setData(x);
--- SumOfUserData.java
+++ SumOfUserData.java
@@ -11,12 +11,15 @@ public class SumOfUserData {
     int calculate() {
         return a + b;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
         int x = sc.nextInt();
+        System.out.println("Enter the second number: ");
         int y = sc.nextInt();
         SumOfUserData sum = new SumOfUserData();
-        sum.setData(x,y);
-        System.out.println("The sum is "+sum.calculate());
+        sum.setData(x, y);
+        System.out.println("The sum is " + sum.calculate());
     }
 }
